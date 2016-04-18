3c277274071f897aff49e550ebf56b368d0e72ff
optimusprike <pudhucodep@gmail.com>
2016-04-18 17:23:18 -0400

    Added files via upload
    credits

---
+++ textad.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+	cout << "CREDITS" << endl;
+	cout << "This project was created for a Programming Fundamentals 2 (COP3503) assignment" << endl;
+	cout << "Written in C++" << endl;
+	cout << " " << endl;
+	cout << "Lead Programmers:" << setw(15) << "Kareem Hunte" << endl;
+	cout << setw(31) << "Alex Brandt" << endl;
+	cout << " " << endl;
+	cout << "Project Managers:" << setw(20) << "Prakash Pudhucode" << endl;
+	cout << " " << setw(30) << "Stanley Tan" << endl;
+	cout << " " << endl;
+	cout << "Testers:" << setw(24) << "Pryana Lewis" << endl;
+	cout << setw(34) << "Arthur Johnson" << endl;
+	cout << " " << endl;
+	cout << "Creative Director:" << setw(16) << "Arthur Johnson" << endl;
+	cout << " " << endl;
+	cout << "Level Designers: " << setw(20) << "Prakash Pudhucode" << endl;
+	cout << setw(34) << "Arthur Johnson" << endl;
+	cout << " " << endl;
+	cout << "Programmers:" << setw(20) << "Pryana Lewis" << endl;
+	cout << setw(37) << "Prakash Pudhucode" << endl;
+	cout << setw(31) << "Stanley Tan" << endl;
+	cout << setw(31) << "Alex Brandt" << endl;
+	cout << setw(34) << "Arthur Johnson" << endl;
+	cout << setw(32) << "Kareem Hunte" << endl;
+	cout << " " << endl; 
+
+
+}
